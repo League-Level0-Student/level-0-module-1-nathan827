3836e472755c5d5d4f4d42b4b830b32e8a66fe6d
student <student@ip-172-31-62-0.us-west-2.compute.internal>
2020-12-03 18:29:27 -0800

    hi

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,35 @@
-
+PImage pepperoni;
+PImage olive;
+PImage mushroom;
 void setup() {
+  size(500,500);
+  fill(#C69324);
+  ellipse(250,250,250,250);
+  fill(255,0,0,255);
+  ellipse(250,250,225,225);
+  fill(#F4FC0F);
+  ellipse(250,250,210,210);
+  pepperoni = loadImage("pepperoni.png");
+  pepperoni.resize(30,30);
+  olive = loadImage("olive.png");
+  olive.resize(15,15);
+  mushroom = loadImage("mushroom.png");
+  mushroom.resize(20,20);
     
 }
 void draw() {
-    
+    image(pepperoni,200,200);
+    image(pepperoni,260,180);
+    image(pepperoni,290,280);
+    image(pepperoni,240,250);
+    image(pepperoni,190,290);
+    image(pepperoni,170,240);
+    image(olive, 230,160);
+    image(olive, 300,220);
+    image(olive,250,300);
+    image(olive, 170,190);
+    image(mushroom, 250,220);
+    image(mushroom, 230,320);
+    image(mushroom, 210,250);
+    image(mushroom, 320,250);
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -2,11 +2,32 @@ package _04_int._1_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
+		
+		int score = 0;
+		
+		String input = JOptionPane.showInputDialog("Brothers and Sisters I have none, but this man's father is my father's son. Who is the man?");
+		
+		if(input.equals("your son")){
+			JOptionPane.showMessageDialog(null,"correct!");
+			score += 1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null,"wrong");
+		}
+		String riddle = JOptionPane.showInputDialog("What is greater than God, more evil than the devil, the poor have it, the rich need it, and if you eat it, you die?");
+		if(riddle.equals("nothing")) {
+			JOptionPane.showMessageDialog(null,"correct!");
+			score += 1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "wrong");
+		}
+		JOptionPane.showMessageDialog(null, "you have " +score+ " points");
 
 		// 1. Make a variable to hold the score
 
